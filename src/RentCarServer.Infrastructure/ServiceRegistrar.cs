cd706b911d3a75930550fe5a44db67f84d84c4bd
onurabdulaji <onurabdulaji@gmail.com>
2025-12-13 10:35:52 +0100

    Infra part -  1

---
+++ src/RentCarServer.Infrastructure/ServiceRegistrar.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RentCarServer.Infrastructure.Context;
+using Scrutor;
+
+namespace RentCarServer.Infrastructure;
+
+public static class ServiceRegistrar
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection service, IConfiguration configuration)
+    {
+        service.AddDbContext<ApplicationDbContext>(options =>
+        {
+            var con = configuration.GetConnectionString("SqlServer")!;
+            options.UseSqlServer(con);
+        });
+
+        service.Scan(action => action.FromAssemblies(typeof(ServiceRegistrar).Assembly).AddClasses(publicOnly: false).UsingRegistrationStrategy(RegistrationStrategy.Skip).AsImplementedInterfaces().WithScopedLifetime());
+
+        return service;
+    }
+}
---
+++ src/RentCarServer.Infrastructure/Services/UserContext.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using RentCarServer.Application.Services;
+
+namespace RentCarServer.Infrastructure.Services;
+
+internal class UserContext(HttpContextAccessor httpContextAccessor) : IUserContext
+{
+    public Guid GetUserId()
+    {
+        var httpContext = httpContextAccessor.HttpContext;
+
+        var claims = httpContext.User.Claims;
+
+        string? userId = claims.FirstOrDefault(q => q.Type == ClaimTypes.NameIdentifier)?.Value;
+
+        if (userId is null)
+        {
+            throw new ArgumentNullException("Kullanici bilgisi bulunamadi");
+        }
+
+        try
+        {
+            var id = Guid.Parse(userId);
+            return id;
+
+        }
+        catch (Exception)
+        {
+            throw new ArgumentNullException("Kullanici Uygun GUID Formatinda Degil !");
+        }
+    }
+}
