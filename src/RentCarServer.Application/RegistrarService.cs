3101a7ceaf83b4c6242f88b2eb5769f69799242c
onurabdulaji <onurabdulaji@gmail.com>
2025-12-13 10:08:36 +0100

    RegistrarService

---
+++ src/RentCarServer.Application/RegistrarService.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using TS.MediatR;
+
+namespace RentCarServer.Application;
+
+public static class RegistrarService
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(typeof(RegistrarService).Assembly);
+        });
+
+        services.AddValidatorsFromAssembly(typeof(RegistrarService).Assembly);
+        
+        return services;
+    }
+}
